611ff37cb5461dcc435be50446229e7107ec55df
Gravey <david.dahlgren92@gmail.com>
2015-07-13 17:18:15 -0700

    Added environment setup script

---
+++ setup-env.sh
@@ -0,0 +1,10 @@
+apt-get update
+apt-get install git -y
+apt-get install libbluetooth-dev -y
+apt-get install python-setuptools -y
+apt-get install python-dev -y
+apt-get install python-glade2 -y
+cd ~/
+git clone https://github.com/karulis/pybluez.git
+cd pybluez
+python setup.py install
